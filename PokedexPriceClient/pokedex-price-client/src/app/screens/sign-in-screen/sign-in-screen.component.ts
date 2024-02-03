0c9e8abe9f2185dc0d6840d5e8e8a867f060fbf0
LucasDechaumet <lucasdechaumet@outlook.com>
2024-02-03 01:56:39 +0100

    add connexion and back

--- PokedexPriceClient/pokedex-price-client/src/app/screens/sign-in-screen/sign-in-screen.component.ts
+++ PokedexPriceClient/pokedex-price-client/src/app/screens/sign-in-screen/sign-in-screen.component.ts
@@ -2,19 +2,34 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Register } from '../../interfaces/register';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-sign-in-screen',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatIconModule],
   templateUrl: './sign-in-screen.component.html',
   styleUrl: './sign-in-screen.component.scss',
 })
 export class SignInScreenComponent {
-  constructor(private authService: AuthService) {}
+  iconVisibility: string = 'visibility_off';
+  visible: boolean = false;
+
+  constructor(private authService: AuthService, private location: Location) {}
 
   onSubmit(formData: Register) {
     console.log('Données du formulaire :', formData);
     this.authService.signIn(formData);
+  }
+
+  viewPassword() {
+    this.iconVisibility = this.visible ? 'visibility_off' : 'visibility';
+    this.visible = !this.visible;
+  }
+
+  close() {
+    this.location.back();
   }
 }
